d3a8f67cc8a14fc414def6203f1e8472c7dd432d
cherakaevs <57588181+cherakaevs@users.noreply.github.com>
2020-10-28 16:46:19 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ text = srcfile.read()
 
 srcfile.close()
 
-num1 = r"\b[7-8]\d{10}"
-num2 = r"\b9\d{9}"
+num1 = r"[7-8]\d{10}"
+num2 = r"9\d{9}"
 
 results = re.findall(num1, text) + re.findall(num2, text)
 
